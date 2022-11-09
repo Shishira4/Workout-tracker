3f1a08c55ddfe025d0455f517fd57f416040daef
Shishira4 <shivashishira@gmail.com>
2022-11-09 15:35:30 +0530

    setting up express

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const express = require('express')
 
 const app = express()//creats an express app
 
-
+//middleware
+app.use((req, res, next)=>{
+    console.log(req.path, req.method)
+    next()
+})
 //route handler to react to requests
 app.get('/',(req, res)=>(
     res.json({mssg: 'Welcome to the app'})
@@ -13,4 +17,4 @@ app.get('/',(req, res)=>(
 app.listen(process.env.PORT,()=>(
     console.log('Listening to port 4000!!hihih')
 ))
-process.env
+process.env 
